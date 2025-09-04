737c01b3a4e53eb2a25313d99c16a2c783a2973b
Suryanshu Nayak <nayaksuryanshu05@gmail.com>
2025-09-04 08:42:51 +0530

    resolve

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_URL || "https://e-commerce-backend-production-5d11.up.railway.app";
 
 export const apiConfig = {
   baseURL: API_BASE_URL,
